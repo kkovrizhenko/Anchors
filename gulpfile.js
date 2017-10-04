cc3d132a0f7e41e896e18313d7a0732f3c788cf9
Serhii Kondratiev <etokub@gmail.com>
2017-10-04 16:40:01 +0300

    sass starting

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,63 @@
-const gulp = require('gulp');
-const fileinclude = require('gulp-file-include');
-const imagemin = require('gulp-imagemin');
-var concat = require('gulp-concat');
- 
+var gulp 			= require('gulp'),
+	fileinclude 	= require('gulp-file-include'),
+	minifyCSS 		= require('gulp-minify-css'),
+	imagemin 		= require('gulp-imagemin'),
+	concat 			= require('gulp-concat'),
+	sass 			= require('gulp-sass'),
+	autoprefixer 	= require('gulp-autoprefixer'),
+	browserSync 	= require('browser-sync');
+
 gulp.task('template', function() {
-	// які файли читати
 	gulp.src('./templates/index.html')
-		.pipe(fileinclude({
-			prefix: '@@',
-			basepath: '@file'
-		}))
-		// куди посістити результат
-		.pipe(gulp.dest('./'));
+	.pipe(fileinclude({
+		prefix: '@@',
+		basepath: '@file'
+	}))
+	.pipe(gulp.dest('./'))
+	.pipe(browserSync.reload({stream: true}))
 });
 
-gulp.task('css', function() {
-  return gulp.src(['./templates/css/reset.css', './templates/css/base.css', './templates/css/heder.css', './templates/css/why.css', './templates/css/latest-projects.css', 'templates/css/footer.css'])
-    .pipe(concat('main.css'))
-    .pipe(gulp.dest('./css/'));
+gulp.task('sass', function(){
+	return gulp.src(['templates/sass/**/*.scss', 'templates/sass/**/*.sass'])
+	.pipe(sass())
+	.pipe(autoprefixer(['last 15 versions'], {cascade: true}))
+	.pipe(gulp.dest('./templates/css/'))
+	.pipe(browserSync.reload({stream: true}))
 });
- 
-gulp.task('img:minify', () =>
-    gulp.src('./img/**/*')
-        .pipe(imagemin())
-        .pipe(gulp.dest('dist/img'))
-);
+
+gulp.task('styles', function() {
+	return gulp.src('templates/css/*.css')
+	.pipe(concat('main.min.css'))
+	.pipe(minifyCSS({
+		keepBreaks: true
+	}))
+	.pipe(gulp.dest('./css/'))
+	.pipe(browserSync.reload({stream: true}))
+});
+
+gulp.task('browser-sync', function(){
+	browserSync({
+		server: {
+			baseDir: './'
+		},
+		notify: false
+	});
+});
+
+gulp.task('watch', ['browser-sync', 'template', 'sass', 'styles'], function(){
+	gulp.watch('templates/sass/**/*', ['sass']);
+	gulp.watch('templates/css/**/*', ['styles']);
+	gulp.watch('templates/*.html', ['template']);
+});
+
+// gulp.task('css', function() {
+//   return gulp.src(['./templates/css/reset.css', './templates/css/base.css', './templates/css/heder.css', './templates/css/why.css', './templates/css/latest-projects.css', 'templates/css/footer.css'])
+//     .pipe(concat('main.css'))
+//     .pipe(gulp.dest('./css/'));
+// });
+
+// gulp.task('img:minify', () =>
+// 	gulp.src('./img/**/*')
+// 	.pipe(imagemin())
+// 	.pipe(gulp.dest('dist/img'))
+// 	);
