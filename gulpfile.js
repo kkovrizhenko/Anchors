2441a5bcdddb5c8b9f32244f718de30df4b6f6f1
Serhii Kondratiev <etokub@gmail.com>
2017-10-07 13:55:21 +0300

    Kolya polomal

--- gulpfile.js
+++ gulpfile.js
@@ -19,17 +19,17 @@ gulp.task('template', function() {
 });
 
 gulp.task('sass', function(){
-	return gulp.src(['templates/sass/**/*.scss', 'templates/sass/**/*.sass'])
+	return gulp.src(['./sass/**/*.scss', './sass/**/*.sass'])
 	.pipe(sass())
 	.pipe(autoprefixer(['last 15 versions'], {cascade: true}))
 	.pipe(sourcemaps.write())
-	.pipe(gulp.dest('./templates/css/'))
+	.pipe(gulp.dest('./css/'))
 	.pipe(browserSync.reload({stream: true}))
 });
 
 gulp.task('styles', function() {
-	return gulp.src('templates/css/*.css')
-	.pipe(concat('main.min.css'))
+	return gulp.src('./css/*.css')
+	.pipe(concat('style.min.css'))
 	.pipe(minifyCSS({
 		keepBreaks: true
 	}))
@@ -47,8 +47,8 @@ gulp.task('browser-sync', function(){
 });
 
 gulp.task('watch', ['browser-sync', 'template', 'sass', 'styles'], function(){
-	gulp.watch('templates/sass/**/*', ['sass']);
-	gulp.watch('templates/css/**/*', ['styles']);
+	gulp.watch('./sass/**/*', ['sass']);
+	// gulp.watch('templates/css/**/*', ['styles']);
 	gulp.watch('templates/*.html', ['template']);
 });
 
