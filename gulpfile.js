2fbaaef1fa52f16dcd22611e4d4602f8823e9bc8
Serhii Kondratiev <etokub@gmail.com>
2017-10-09 16:36:12 +0300

    new plugins in gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 var gulp 			= require('gulp'),
 	fileinclude 	= require('gulp-file-include'),
 	minifyCSS 		= require('gulp-minify-css'),
-	merge = require('merge-stream'),
+	merge 			= require('merge-stream'),
 	imagemin 		= require('gulp-imagemin'),
 	concat 			= require('gulp-concat'),
 	sass 			= require('gulp-sass'),
@@ -9,6 +9,8 @@ var gulp 			= require('gulp'),
 	sourcemaps 		= require('gulp-sourcemaps'),
 	browserSync 	= require('browser-sync'),
 	spritesmith 	= require('gulp.spritesmith'),
+	imagemin 		= require('gulp-imagemin'),
+	cache			= require('gulp-cache'),
 	del 			= require('del');
 
 gulp.task('template', function() {
@@ -65,15 +67,42 @@ gulp.task('sprite', () => {
 
     return merge(imgStream, cssStream);
 });
+// зтиснення svg, png, jpeg
+gulp.task('imgmin', function(){
+    // беремо всі картинки крім папки де лежать картинки для спрайту
+    // картинки зі спрайту упаковуються в окрему картинку
+    return gulp.src(['./img/**/*', '!./pic/*'])
+        .pipe(cache(imagemin({
+
+        })))
+        .pipe(gulp.dest('./dist/img'));
+});
 
 gulp.task('clean', function(){
-	return del.sync('./index.html');
+	return del.sync('./dist');
+});
+gulp.task('clean', function(){
+	return cache.clearAll();
 });
 
 gulp.task('watch', ['browser-sync', 'template', 'sass', 'styles'], function(){
 	gulp.watch('./sass/**/*', ['sass']);
 	// gulp.watch('templates/css/**/*', ['styles']);
 	gulp.watch('templates/*.html', ['template']);
+});
+
+
+gulp.task('build', ['clean', 'sass', 'template', 'imgmin'], function(){
+	var buildCss = gulp.src([
+		'css/main.css'
+	])
+		.pipe(gulp.dest('dist/css'));
+
+	var buildHTML = gulp.src('./*.html')
+		.pipe(gulp.dest('dist/'));
+
+	var buildPic= gulp.src('./pic/**/*')
+		.pipe(gulp.dest('dist/pic'));	
 });
 
 // gulp.task('css', function() {
